85d7f2a76262aaaa546b453e8ca521e3783eb0a5
Felipe <felipe@MacBook-Pro-de-Felipe-2.local>
2020-03-30 11:34:17 -0300

    Primer git y clase cancion

---
+++ Laboratorio2/Cancion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Laboratorio2
+{
+    public class Cancion
+    {
+        private string name;
+        private string album;
+        private string artist;
+        private string gender;
+
+        public Cancion(string name, string album, string artist, string gender)
+        {
+            this.name = name;
+            this.album = album;
+            this.artist = artist;
+            this.gender = gender;
+        }
+
+        public String Informacion()
+        {
+            return $" genero: {gender}, artista: {artist}, album: {album}, nombre: {name}";
+        }
+
+        public String Name
+        {
+            get { return name; }
+        }
+
+        public String Album
+        {
+            get { return album; }
+        }
+
+        public String Artist
+        {
+            get { return artist; }
+        }
+
+        public String Gender
+        {
+            get { return gender; }
+        }
+    }
+    
+}
---
+++ Laboratorio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop)
+            {
+               Console.Clear();
+               Console.WriteLine("Hola Bienvenido Espotifai");
+                loop = false;
+
+            }
+            
+
+        }
+    }
+}
